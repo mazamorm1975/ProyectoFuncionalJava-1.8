37984586c8d8b9aee0626010d8bf7373ea153999
mazamora1975 <mzamora114@hotmail.com>
2024-01-02 10:28:15 -0600

    Streams 02/01/2024

---
+++ src/main/java/org/example/streams/AppStream.java
@@ -0,0 +1,26 @@
+package org.example.streams;
+
+import java.time.LocalDate;
+
+public class AppStream {
+
+    public static void main(){
+
+        Client e1 = new Client(1, "Client1", "Trainee Developer", LocalDate.of(1991, 1, 1), 1000.00, "Peru");
+        Client e2 = new Client(2, "Client2", "QA", LocalDate.of(1993, 2, 1), 2000.00, "Peru");
+        Client e3 = new Client(3, "Client3", "Architect", LocalDate.of(1995, 3, 1), 3000.00, "Paraguay");
+        Client e4 = new Client(4, "Client4", "Cloud Engineer", LocalDate.of(1987, 4, 1), 4000.00, "Colombia");
+        Client e5 = new Client(5, "Client5", "Cobol Engineer", LocalDate.of(1956, 1, 1), 5000.00, "Argentina");
+        Client e6 = new Client(6, "Client6", "Scrum Master", LocalDate.of(2002, 11, 1), 4500.00, "Argentina");
+        Client e7 = new Client(7, "Client7", "Leader Project", LocalDate.of(1998, 12, 1), 10000.00, "Mexico");
+        Client e8 = new Client(8, "Client8", "Senior Developer", LocalDate.of(1996, 7, 1), 7000.00, "Rep. Dominicana");
+        Client e9 = new Client(9, "Client9", "Junior Developer", LocalDate.of(2008, 8, 1), 500.00, "Ecuador");
+        Client e10 = new Client(10, "Client10", "Mobile Developer", LocalDate.of(1975, 9, 1), 3000.00, "Chile");
+        Client e11 = new Client(11, "Client11", "Accounting", LocalDate.of(1985, 7, 3), 2000.00, "España");
+        Client e12 = new Client(12, "Client12", "Manager", LocalDate.of(1980, 9, 2), 2000.00, "Mexico");
+        Client e13 = new Client(13, "Client13", "Manager II", LocalDate.of(1980, 9, 2), 2000.00, "Peru");
+        Client e14 = new Client(14, "Client23", "Manager III", LocalDate.of(1980, 9, 2), 2000.00, "Peru");
+
+
+    }
+}
