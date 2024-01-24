ae4b51576524946d998805729ea67fa1879464f1
mazamora1975 <mzamora114@hotmail.com>
2024-01-24 14:19:23 -0600

    Se analizan clases de tipo record y clases selladas para delimitar la herencia entre clases e interfaces

---
+++ src/main/java/org/java_12_17/Fabric2.java
@@ -0,0 +1,6 @@
+package org.java_12_17;
+
+public interface Fabric2 {
+
+    void testFabric2();
+}
---
+++ src/main/java/org/java_12_17/Fabric3.java
@@ -0,0 +1,6 @@
+package org.java_12_17;
+
+public interface Fabric3 {
+
+    void testFabric3();
+}
